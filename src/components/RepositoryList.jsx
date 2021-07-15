c615ee97703d1107e7a21adb2074baaa2fe71cd3
romulorocha08 <romulosilva0819@gmail.com>
2021-07-15 02:27:24 -0300

    componente react

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,37 @@
+const repositoryName = 'unform2';
+
+export function RepositoryList() {
+ return(
+  <section className="repository-list">
+    <h1>Lista de repositorio</h1>
+
+    <ul>
+      <li>
+        <strong>{repositoryName}</strong>
+        <p>Forms in React</p>
+
+        <a href="">
+          Acssar repositórios
+        </a>
+      </li>
+      <li>
+        <strong>unform</strong>
+        <p>Forms in React</p>
+
+        <a href="">
+          Acssar repositórios
+        </a>
+      </li>
+      <li>
+        <strong>unform</strong>
+        <p>Forms in React</p>
+
+        <a href="">
+          Acssar repositórios
+        </a>
+      </li>
+    </ul>
+  </section>
+ );
+
+}
